c70377b83c524858c4dbe5603fc588ed672273e5
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-07 16:58:45 +0700

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text,num) {
+    if (num < 0) return "ERROR"
+    let finalText = "";
+    for (let i = 0; i < num;i++ ){
+        finalText += text ;
+    }
+    return finalText;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
