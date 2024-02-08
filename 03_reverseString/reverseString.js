91a32e14632fcbb636241090763290f823098eb7
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-08 14:28:11 +0700

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+ const arrayWord = word.split("");
+ const revWord = [];
 
+ for (let i = arrayWord.length - 1; i >= 0; i--){
+    const ValueAtIndex = arrayWord[i]
+    revWord.push(ValueAtIndex)
+ }
+ return revWord.join("");
 };
 
 // Do not edit below this line
