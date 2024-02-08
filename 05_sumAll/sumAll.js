0d96e091745d4b775d3e430d483e2fa28a06e4a8
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-08 15:48:57 +0700

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(startNum,endNum) {
 
+    if (typeof startNum !== "number" || typeof endNum !== "number"){
+        return "ERROR"
+    }
+
+    if (startNum > endNum){
+        let endNumTemp = startNum;
+        startNum = endNum;
+        endNum = endNumTemp;
+    }
+
+    if (startNum < 0 || endNum < 0){
+        return "ERROR"
+    }
+
+
+
+
+    let numList = [];
+    for (i = endNum - 1; i > startNum;i--){
+        numList.push(i)
+    }
+    let numListSum = numList.reduce((a,b) => a + b,0);
+    let totalSum = numListSum + startNum + endNum;
+    return totalSum;
 };
 
 // Do not edit below this line
