bdb5803c00c350c1fb3b54769638d630e2777411
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-26 14:56:03 +0700

    Finish 09
    To deal with a string that has multiple words, remove all
    punctuations from the string split the string and reverse it.
    After that join them back together and split them again by space.
    Then join them again to get a reversed string with without spaces and
    punctuations.Then lowercase them.
    
    Finally, during comparisons remove all punctuations, and space. Then
    add lowercase and compare both original and reversed string.
    
    It should work perfectly fine.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuation = /[\.,?!]/g;
+    let reverseString = string.replace(punctuation,"").split("").reverse().join("").toLowerCase().split(" ").join("")
+    console.log(reverseString)
 
+    if (string.replace(punctuation,"").toLowerCase().split(" ").join("") === reverseString){
+        return true
+    }else{
+        return false
+    }
 };
+console.log(palindromes("A car, a man, a maraca."))
 
 // Do not edit below this line
 module.exports = palindromes;
