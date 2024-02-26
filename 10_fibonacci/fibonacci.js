010619803cf12bfc8a75bdfbd3db514f8e9cf2db
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-26 15:26:59 +0700

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fiboArr = [1,1]
+
+    if (Number(num) === 0){
+        return 0
+    }
+
+    if (num < 0){
+        return "OOPS"
+    }
+    
+    for (let i = 0; i < Number(num);i++){
+        let sum = fiboArr[i] + fiboArr[i + 1]
+        let FinalFibo = fiboArr.push(sum)
+    }
+    return fiboArr[num - 1];
 
 };
 
